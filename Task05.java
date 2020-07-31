7491293cc85a8525b76374b03e71d19adad937e2
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-31 14:58:34 +0200

    Update Task05.java

--- Task05.java
+++ Task05.java
@@ -9,14 +9,17 @@ public class Task05 {
 		
 		int[][]evenStrings = new int[10][9];
 		
-		for(int i = 2; i< evenStrings.length;  i = i + 2 ) {
+		      for(int i = 2; i< evenStrings.length;  i = i + 2 ) {
 			
-			for(int j = 0; j< evenStrings[i].length; j++) {
+			    for(int j = 0; j< evenStrings[i].length; j++) {
 				
-				System.out.print(evenStrings[i][j] + "");
-			}
-			System.out.println();
-		}	
-	}
-	
-}
+				  System.out.print(evenStrings[i][j] + "");
+			             }
+			                 System.out.println();
+						}	
+						}
+						}
+						//000000000
+						//000000000
+						//000000000
+						//000000000

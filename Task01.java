344bfc2fdac1cb9fc2ac6c79503fd95a2a88ae97
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-31 13:10:49 +0200

    Add files via upload

---
+++ Task01.java
@@ -0,0 +1,14 @@
+package by.htp.cycles.logic;
+
+public class Task01 {
+public static void task() {
+	//Необходимо вывести на экран числа от 1 до 5.
+	
+	//int i;
+	 
+	for(int i=1; i<6; i++) {
+		
+		System.out.print(i+" ");
+	}
+}
+}

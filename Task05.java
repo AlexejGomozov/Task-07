5982cd38e0a3aea20860691460fdaa87c8f48689
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-31 14:57:26 +0200

    Add files via upload

---
+++ Task05.java
@@ -0,0 +1,22 @@
+package by.htp.arraysofarrays.logic;
+
+/* Дана матрица. Вывести на экран все четные строки, то есть с четными номерами. 
+ * A matrix is given. Display all even lines, that is, with even numbers.*/
+
+public class Task05 {
+
+	public static void main(String[]args) {
+		
+		int[][]evenStrings = new int[10][9];
+		
+		for(int i = 2; i< evenStrings.length;  i = i + 2 ) {
+			
+			for(int j = 0; j< evenStrings[i].length; j++) {
+				
+				System.out.print(evenStrings[i][j] + "");
+			}
+			System.out.println();
+		}	
+	}
+	
+}

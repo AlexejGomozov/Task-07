e86ed34b99b368b4d5fcc8e390a8e9321901aef8
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-31 14:44:02 +0200

    Update Task04.java

--- Task04.java
+++ Task04.java
@@ -3,20 +3,22 @@ package by.htp.arraysofarrays.logic;
 /* Дана матрица. Вывести на экран первую и последнюю строки. 
  * A matrix is given. Display the first and last lines. */
 
-public class Task04 {
+  public class Task04 {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		
 		int[][] firstLastStrings = new int [3][5];
 		
-		for (int i = 0; i< firstLastStrings.length; i = i + (firstLastStrings.length-1)) {
+		      for (int i = 0; i< firstLastStrings.length; i = i + (firstLastStrings.length-1)) {
 			
-			for(int j = 0; j< firstLastStrings[i].length; j++) {
+			     for(int j = 0; j< firstLastStrings[i].length; j++) {
 				
-				System.out.print(firstLastStrings[i][j] + "");
+				   System.out.print(firstLastStrings[i][j] + "");
 			
-			}
-			System.out.println();
-		}		
-	}
-}
+			                  }
+			                   System.out.println();
+		                             }		
+	                                       }
+                                                 }
+						//00000
+						//00000

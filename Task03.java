e2833780bab96ce2be9e4ecc8fe29c8220902cbb
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-31 14:31:43 +0200

    Add files via upload

---
+++ Task03.java
@@ -0,0 +1,21 @@
+package by.htp.arraysofarrays.logic;
+
+/* Дана матрица. Вывести на экран первый и последний столбцы. 
+ * A matrix is given. Display the first and last columns.*/
+
+public class Task03 {
+
+	public static void main(String[] args) {
+		
+		int [][] matrixFirstLast = new int [4][6];
+		
+		for(int i = 0; i< matrixFirstLast.length; i++) {
+		
+		for (int j = 0; j < matrixFirstLast[i].length; j = j + (matrixFirstLast[i].length-1)) {
+		
+			System.out.print(matrixFirstLast[i][j] + " ");						
+	}		
+		System.out.println();
+}
+}
+}

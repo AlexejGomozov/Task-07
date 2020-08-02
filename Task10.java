87436b05819d313679801ecbda4db11e75c22cab
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-02 21:32:41 +0200

    Update Task10.java

--- Task10.java
+++ Task10.java
@@ -5,24 +5,23 @@ package by.htp.arraysofarrays.logic;
 
 public class Task10 {
 	
-public static void main(String[] args) {
+  public static void main(String[] args) {
 	
-	int x = 6;
+     int x = 6;
 	
-	int y = 6;
+        int y = 6;
 	
-	int [][] rowColumnMatrix = new int[x][y];
+	  int [][] rowColumnMatrix = new int[x][y];
 	
-	 for(int i = 0; i< x; i++ ) {
+	     for(int i = 0; i< x; i++ ) {
 			
-		 for(int j = 0; j< y; j++) {
+		for(int j = 0; j< y; j++) {
 				
-			 rowColumnMatrix[i][j]= (int)(Math.random()*10);
+	           rowColumnMatrix[i][j]= (int)(Math.random()*10);
 				
-				System.out.print(rowColumnMatrix[i][j]+ " ");
-				
-			      }
-			       System.out.println();
+		      System.out.print(rowColumnMatrix[i][j]+ " ");			
+			 }
+			  System.out.println();
 		             }
 		               System.out.println("___________");
 	
@@ -30,15 +29,32 @@ public static void main(String[] args) {
 		            	
 		            	    for(int j= 0; j< y; j++) {
 		            				            		
-		            		  if (j == 3) 
-		            			  System.out.print(rowColumnMatrix[i][3]+ " ");
-		            		  if (j == 3 & i == 3) continue;          //строка и столбец красиво получаются, крестом(два числа не повторяются)
-		            		     if (i == 3)
-		            		    	 System.out.print(rowColumnMatrix[3][j]+ " ");
+		            		 if (j == 3)
+						 
+		            	           System.out.print(rowColumnMatrix[i][3]+ " ");
+					    
+		            		      if (j == 3 & i == 3) continue;          //строка и столбец красиво получаются, крестом(два числа не повторяются)
+					    
+		            		         if (i == 3)
+							 
+		            		    	   System.out.print(rowColumnMatrix[3][j]+ " ");
 		            		
-		            		       else  System.out.print("  ");
-		            	            }
-		            	             System.out.println();
-		                               }		               
-                                        }
-										  } 
+		            		              else  System.out.print("  ");
+		            	                        }
+		            	                         System.out.println();
+		                                           }		               
+                                                             }
+						              } 
+//2 5 3 8 2 9 
+//1 1 6 3 6 0 
+//8 4 8 1 5 1 
+//5 2 4 0 5 0 
+//3 1 5 2 3 2 
+//7 1 0 8 6 7 
+//___________
+//      8       
+//      3       
+//      1       
+//5 2 4 0 5 0 
+//      2       
+//      8       

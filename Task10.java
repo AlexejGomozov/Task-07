08b7f513277f9861db4191d43a1f87ab2e67e5c9
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-02 21:29:38 +0200

    Add files via upload

---
+++ Task10.java
@@ -0,0 +1,44 @@
+package by.htp.arraysofarrays.logic;
+
+/* Дана матрица. Вывести k-ю строку и p-й столбец матрицы.
+ *  A matrix is given. Print the k-th row and p-th column of the matrix. */
+
+public class Task10 {
+	
+public static void main(String[] args) {
+	
+	int x = 6;
+	
+	int y = 6;
+	
+	int [][] rowColumnMatrix = new int[x][y];
+	
+	 for(int i = 0; i< x; i++ ) {
+			
+		 for(int j = 0; j< y; j++) {
+				
+			 rowColumnMatrix[i][j]= (int)(Math.random()*10);
+				
+				System.out.print(rowColumnMatrix[i][j]+ " ");
+				
+			      }
+			       System.out.println();
+		             }
+		               System.out.println("___________");
+	
+		                 for(int i = 0; i< x; i++) {
+		            	
+		            	    for(int j= 0; j< y; j++) {
+		            				            		
+		            		  if (j == 3) 
+		            			  System.out.print(rowColumnMatrix[i][3]+ " ");
+		            		  if (j == 3 & i == 3) continue;          //строка и столбец красиво получаются, крестом(два числа не повторяются)
+		            		     if (i == 3)
+		            		    	 System.out.print(rowColumnMatrix[3][j]+ " ");
+		            		
+		            		       else  System.out.print("  ");
+		            	            }
+		            	             System.out.println();
+		                               }		               
+                                        }
+										  } 

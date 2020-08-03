c263f421e9cfbacc2eebb33380037b711aab3615
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-03 15:58:12 +0200

    Update Task17.java

--- Task17.java
+++ Task17.java
@@ -6,42 +6,42 @@ package by.htp.arraysofarrays.logic;
  *   so that cells with "1" go along the perimeter of the square */
 
  public class Task17 {
-
-	public static void main(String[]args) {
+	 
+     public static void main(String[]args) {
 		
-	  int n = 5;
+	int n = 5;
 		
-		int per = 1;
+            int per = 1;
 		
-		  int [][] goAlongPerimeter = new int[n][n];
+		int [][] goAlongPerimeter = new int[n][n];
 		
 		    for (int i = 0; i< n; i++) {
 			
-			   for(int j = 0; j< n; j++) {
+			for(int j = 0; j< n; j++) {
 				
-				 System.out.print(goAlongPerimeter[i][j] + " ");											
+		            System.out.print(goAlongPerimeter[i][j] + " ");											
 			       }
-			       System.out.println();
-		             }
-		             System.out.println("_________");
+			        System.out.println();
+		                  }
+		                    System.out.println("_________");
 		
-                       for (int i = 0; i< n; i++) {
+                                       for (int i = 0; i< n; i++) {
 			
-			             for(int j = 0; j< n; j++) {
+			                   for(int j = 0; j< n; j++) {
 										
-				            if(i == 0)    System.out.print(per + " ");  //if(i == 0 & i == (n-1) & j == 0 & j == (n-1))
+				               if(i == 0)    System.out.print(per + " ");  //if(i == 0 & i == (n-1) & j == 0 & j == (n-1))
 				
-				              else if(i == n-1)    System.out.print(per + " ");
+				                  else if(i == n-1)    System.out.print(per + " ");
 				   
-				                else if(j == n-1)    System.out.print(per + " ");
+				                     else if(j == n-1)    System.out.print(per + " ");
 				
-				                  else if(j == 0)    System.out.print(per + " ");
+				                        else if(j == 0)    System.out.print(per + " ");
 				
-				                    else  System.out.print(0 + " ");
-			                          }
-			                          System.out.println();
-		                                }
-	                                      }
+				                          else  System.out.print(0 + " ");
+			                                    }
+			                                     System.out.println();
+		                                               }
+	                                                         }
 	
 }
 //0 0 0 0 0       

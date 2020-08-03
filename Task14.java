9bdb5f33a69f0be4c6065aacb4f38c546344aebc
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-03 12:38:35 +0200

    Update Task14.java

--- Task14.java
+++ Task14.java
@@ -17,23 +17,34 @@ public class Task14 {
 		
 		         for(int i = 0; i< n; i++) {
 			
-			         for(int j = 0; j< n; j++) {
+			     for(int j = 0; j< n; j++) {
 				
-				        System.out.print(diagonallyUpperRightCorner[i][j]  + " "); //n - 1 - i    diagonallyUpperRightCorner[i][j]
+				  System.out.print(diagonallyUpperRightCorner[i][j]  + " "); 
 			             }
 			               System.out.println();
-		                     }
-		                      System.out.println("_________");
+		                         }
+		                            System.out.println("_________");
 		
-		                         for(int i = 0; i< n; i++) {
+		                                for(int i = 0; i< n; i++) {
 			
-			                        for(int j = 0; j< n; j++) {
+			                            for(int j = 0; j< n; j++) {
 				
-				                       if (i == n - 1 - j)  System.out.print(c++  + " "); //n - 1 - i    diagonallyUpperRightCorner[i][j]
+				                       if (i == n - 1 - j)  System.out.print(c++  + " "); //по диагонали из правого верхнего угла
 			
-				                         else System.out.print(0  + " ");
-			                               }
-			                                System.out.println();
-		                         			  	}
-												  }	
-											     	}
+				                          else System.out.print(0  + " ");                //остальные ячейки заполняем '0'
+			                                    }
+			                                      System.out.println();
+		                         			  }
+								     }	
+								        }
+//0 0 0 0 0 
+//0 0 0 0 0 
+//0 0 0 0 0 
+//0 0 0 0 0 
+//0 0 0 0 0 
+//_________
+//0 0 0 0 1 
+//0 0 0 2 0 
+//0 0 3 0 0 
+//0 4 0 0 0 
+//5 0 0 0 0 

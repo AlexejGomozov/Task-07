2606bf1d7d2eade2dd0f9ec52d8041f39e35d360
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-03 16:47:00 +0200

    Update Task18.java

--- Task18.java
+++ Task18.java
@@ -7,37 +7,36 @@ package by.htp.arraysofarrays.logic;
 
 public class Task18 {
 
-	public static void main (String[]args) {
+  public static void main (String[]args) {
 		
-		int n = 5;
+      int n = 5;
 		
-		int count = 1;
+	 int count = 1;
 		
-	int [][] elementsRowEqualRowNumber = new int[n][n];
+	    int [][] elementsRowEqualRowNumber = new int[n][n];
 		
-		for(int i = 0; i< n; i++) {
+	       for(int i = 0; i< n; i++) {
 			
-			for(int j = 0; j< n; j++) {
+		  for(int j = 0; j< n; j++) {
 				
-				System.out.print(elementsRowEqualRowNumber[i][j] + " ");	
-				
-			}
-			System.out.println();
-		}
-		       System.out.println("_________");
+		     System.out.print(elementsRowEqualRowNumber[i][j] + " ");				
+		       }
+			 System.out.println();
+		            }
+		             System.out.println("_________");
 		
-                  for(int i = 0; i< n; i++) {
+                                for(int i = 0; i< n; i++) {
 			
-			         for(int j = 0; j< n; j++) {
+			           for(int j = 0; j< n; j++) {
 						
-			        	if(i== (count - 1)) {System.out.print(count + " ");	count ++;}
+			              if(i== (count - 1)) {System.out.print(count + " ");	count ++;}
 				
-				           else System.out.print(count - 1 + " ");
-			                 }
-			                  System.out.println();
-		                        }	
-	                              }	
-                                    }
+				          else System.out.print(count - 1 + " ");
+			                     }
+			                      System.out.println();
+		                                }	
+	                                         }	
+                                                   }
 //0 0 0 0 0 
 //0 0 0 0 0 
 //0 0 0 0 0 

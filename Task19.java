cde49142dca4a3c993ad3f0f18dd3ceeb1f61c1a
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-05 12:20:32 +0200

    Update Task19.java

--- Task19.java
+++ Task19.java
@@ -1,55 +1,61 @@
 package by.htp.arraysofarrays.logic;
 
-/* Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):  Form a square matrix of order n according to a given pattern (n is even):
+/* Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):    Form a square matrix of order n according to a given pattern (n is even):
  * "1" по всей ширине матрицы,  с каждой строкой "1" меньше на одну ячейку слева  "1" over the entire width of the matrix, with each row less by one cell 
- * и справа ( две в сумме)следующая строка - 2 ячейки с кажой стороны строки.    to the left and right (two in total) next row - 2 cells on each side of
- *  И так до середины матрицы. Потом от серидины идет обратное увеличение.       the row. And so on until the middle of the matrix. Then there is a reverse 
-                                                                                 increase from the middle.    */
+ * и справа ( две в сумме)следующая строка - 2 ячейки с кажой стороны строки.     to the left and right (two in total) next row - 2 cells on each side of
+ *  И так до середины матрицы. Потом от серидины идет обратное увеличение.        the row. And so on until the middle of the matrix. Then there is a reverse 
+                                                                                  increase from the middle.    */
 public class Task19 {
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		
-	 int n = 12;
+	int n = 12;
 		
-		int count = 0;
+            int count = 0;
 						
-		   int[][]reverseIncreaseFromMiddle = new int[n][n];
+		int[][]reverseIncreaseFromMiddle = new int[n][n];
 		
-		       for(int i= 0; i< n; i++) {
+		   for(int i= 0; i< n; i++) {
 			
-			      for(int j = 0; j< n; j++) {
+		       for(int j = 0; j< n; j++) {
 								
-				     reverseIncreaseFromMiddle[i][j] = 1;
+		         reverseIncreaseFromMiddle[i][j] = 1;
 				
-				         System.out.print(reverseIncreaseFromMiddle[i][j]+ " ");			
-			               }
-			                System.out.println();
-		                      }
-		    System.out.println("_______________________");
+			    System.out.print(reverseIncreaseFromMiddle[i][j]+ " ");			
+			       }
+			        System.out.println();
+		                   }
+		                    System.out.println("_______________________");
 		
-		      for(int i= 0; i< n; i++) {   
+		                       for(int i= 0; i< n; i++) {   
 		    	
-			    for(int j = 0; j< n; j++) {  
+			                  for(int j = 0; j< n; j++) {  
 				  
-				  if(count < (n/2)) { 
+				             if(count < (n/2)) { 
 					  
-					  if ((i == count & j<= i - 1 )|(i == count & j >= n-count)) {
+					        if ((i == count & j<= i - 1 )|(i == count & j >= n-count)) {
 						  
 						  System.out.print(0 + " ");  } else  System.out.print(1 + " ");
-					        }    		    
-				            if(count >=(n/2)) {
+					            }    		    
+				                     if(count >=(n/2)) {
 			  
-			                   if ((i == count & j <= n -2 - i) | (i == count & j >= n - (n - count-1) ))  { 
+			                               if ((i == count & j <= n -2 - i) | (i == count & j >= n - (n - count-1) ))  { 
 			                	   
-			                	   System.out.print(0 + " ");}else  System.out.print(1 + " ");	 
-			                        }
-				  
+			                	          System.out.print(0 + " ");}else  System.out.print(1 + " ");	 
+			                                    }				                            
+					                      }  
+			                                        count++ ;   
+			  
+			                                          System.out.println();
+			                                            } 
+	                                                              }
+                                                                        }
 																 
 	/*			if((i == 1 & j==0 )| (i == 1 & j == n-1)) System.out.print(0 + " "); 
 				
 				  else if((i == 2 & j<=1)| (i == 2 & j >= n-2)) System.out.print(0 + " ");  
 				
-				    else if((i == 3 & j<=2)| (i == 3 & j >= n-3)) System.out.print(0 + " ");      //из этого образца делал цикл с if для матрицы квадрата любой величины 
+				    else if((i == 3 & j<=2)| (i == 3 & j >= n-3)) System.out.print(0 + " ");      //из этого пошагового образца делал цикл с if для матрицы квадрата любой величины 
 				 
 				      else if((i == 4 & j<=2)| (i == 4 & j >= n-3)) System.out.print(0 + " "); 
 				
@@ -58,13 +64,7 @@ public class Task19 {
 				          else if((i == 6 & j==0)| (i == 6 & j == n-1)) System.out.print(0 + " ");*/
 									  
 			               //  else  System.out.print(1 + " ");				 
-			                 }  
-			                  count++ ;   
-			  
-			                  System.out.println();
-			                   } 
-	                            }
-                                 }
+			                
 //1 1 1 1 1 1 1 1 1 1 1 1 
 //1 1 1 1 1 1 1 1 1 1 1 1 
 //1 1 1 1 1 1 1 1 1 1 1 1 

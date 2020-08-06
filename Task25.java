fe17222583e9d5c894802f5abdabe8b8389d466e
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-06 19:49:13 +0200

    Update Task25.java

--- Task25.java
+++ Task25.java
@@ -1,31 +1,31 @@
 package by.htp.arraysofarrays.logic;
 
-/*  Получить квадратную матрицу порядка n:            								Get a square matrix of order n: on a new line, each element is 
+/*  Получить квадратную матрицу порядка n:            				     Get a square matrix of order n: on a new line, each element is 
  *  в новой строке каждый элемент умножаем на множетель равный номеру строки + 1     multiplied by a multiplier equal to the line number  + 1.   */
                                                                                   
 
-public class Task25 {
+          public class Task25 {
 
-	public static void main(String[] args) {
+	     public static void main(String[] args) {
 		
 		int n = 5;
 		
 		  int[][]multiplierEqualLineNumber = new int[n][n];
 		
-		    for(int i = 0; i< n; i++) {
+		     for(int i = 0; i< n; i++) {
 			
-			  int increase = 1;
+			 int increase = 1;
 			
 			    for(int j = 0; j< n; j++) {
 				
 			    	multiplierEqualLineNumber[i][j] = increase++; 
 				
-				    System.out.print(multiplierEqualLineNumber[i][j] * (i+1) + "   ");				
-			          }			
-			           System.out.println();
-		}				
-	}
-	}
+				   System.out.print(multiplierEqualLineNumber[i][j] * (i+1) + "   ");				
+			             }			
+			              System.out.println();
+		                    }				
+	                          }
+	                        }
 
 //1   2   3   4   5   
 //2   4   6   8   10   

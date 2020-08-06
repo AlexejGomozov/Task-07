f4e24f39f4551c9bbc8c53fe5ad715e974ab52fb
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-06 19:34:55 +0200

    Update Task24.java

--- Task24.java
+++ Task24.java
@@ -3,28 +3,28 @@ package by.htp.arraysofarrays.logic;
 /*  Дан линейный массив x1 , x2, x3... , xn . Получить действительную квадратную матрицу порядка :    You are given a linear array x1 , x2, x3... , xn. Get real square order matrix: 
  *  каждая новая строка с повторяющимися элементами в степени на одну больше  предыдущей строки.   each new row with repeating elements is one power greater than the previous row */
 
-public class Task24 {
+        public class Task24 {
 
-	public static void main(String[]args) {
+	    public static void main(String[]args) {
 		
 		int n = 5;
 		
 		  int[][]elementsOnePowerGreater = new int[n][n];
 		
-		    for(int i = 0; i< n; i++) {
+		     for(int i = 0; i< n; i++) {
 			
-			  int increase = 1;
+			 int increase = 1;
 			
 			    for(int j = 0; j< n; j++) {
 				
-				  elementsOnePowerGreater[i][j] = increase++; 
+				elementsOnePowerGreater[i][j] = increase++; 
 				
 				    System.out.print((int)Math.pow(elementsOnePowerGreater[i][j], i+1) + "   ");				
-			          }			
-			           System.out.println();
-		}				
-	}
-}
+			             }			
+			               System.out.println();
+		                    }				
+	                        }
+                            }
 //1   2   3   4   5   
 //1   4   9   16   25   
 //1   8   27   64   125   

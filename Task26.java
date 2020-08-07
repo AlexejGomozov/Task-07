49986480d871dca94df6a87b32ae12de0c9aca4d
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-08-07 23:11:07 +0200

    Update Task26.java

--- Task26.java
+++ Task26.java
@@ -17,74 +17,74 @@ public class Task26 {
 		
    int m = 7;
 		
-	int count = 0;
+     int count = 0;
 		
-	 int temp1;
+       int temp1;
 		
-	  int[][]sumNegatDetermineMaxRearrangePlaces = new int[n][m];
+	 int[][]sumNegatDetermineMaxRearrangePlaces = new int[n][m];
 		 
 	   int rowMin = 0, colMin = 0, rowMax = 0, colMax = 0;
 		
-		sumNegatDetermineMaxRearrangePlaces[rowMax][colMax] = sumNegatDetermineMaxRearrangePlaces[0][0];  // maxArray задаем макс значение для дальнейшего его нахождения
+	     sumNegatDetermineMaxRearrangePlaces[rowMax][colMax] = sumNegatDetermineMaxRearrangePlaces[0][0];  // maxArray задаем макс значение для дальнейшего его нахождения
 	  	 
-	  	 sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] = sumNegatDetermineMaxRearrangePlaces[0][0]; // minArray  задаем мин значение для дальнейшего его нахождения
+	       sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] = sumNegatDetermineMaxRearrangePlaces[0][0]; // minArray  задаем мин значение для дальнейшего его нахождения
 		
-		  for(int i = 0; i< n; i++ ) {
+		 for(int i = 0; i< n; i++ ) {
 			
 		   for(int j = 0; j< m; j++) {
 				
-			 sumNegatDetermineMaxRearrangePlaces[i][j] = (int)(100 - Math.random()*200);       // задаем массив через рандом с отрицательными числами
+	             sumNegatDetermineMaxRearrangePlaces[i][j] = (int)(100 - Math.random()*200);       // задаем массив через рандом с отрицательными числами
 							
-			  System.out.print(sumNegatDetermineMaxRearrangePlaces[i][j] + " ");			
-			   }		
-			   System.out.println();
-		        }
-		        System.out.println("_____________________________________________________________________________________");
+		      System.out.print(sumNegatDetermineMaxRearrangePlaces[i][j] + " ");			
+		        }		
+			System.out.println();
+		         }
+		          System.out.println("_____________________________________________________________________________________");
 		
 		
-          for(int i = 0; i< n; i++ ) {
+   for(int i = 0; i< n; i++ ) {
         	  
-           int sum = 0;                                                                                       //значение sum обнуляем с новым циклом для нахождения суммы отриц знач в кажд строке
+     int sum = 0;                                                                                       //значение sum обнуляем с новым циклом для нахождения суммы отриц знач в кажд строке
         	   
-        	int max = sumNegatDetermineMaxRearrangePlaces[i][0];                                               //задаем макс значение для дальнейшего его нахождения в каждой отдельной строке
+       int max = sumNegatDetermineMaxRearrangePlaces[i][0];                                               //задаем макс значение для дальнейшего его нахождения в каждой отдельной строке
         	       	  
-		     for(int j = 0; j< m; j++) {
+         for(int j = 0; j< m; j++) {
 									
-	          if(i== count & sumNegatDetermineMaxRearrangePlaces[i][j] < 0)  sum= sumNegatDetermineMaxRearrangePlaces[i][j]+ sum;              //находим сумму отриц значений в каждой строке
+	   if(i== count & sumNegatDetermineMaxRearrangePlaces[i][j] < 0)  sum= sumNegatDetermineMaxRearrangePlaces[i][j]+ sum;              //находим сумму отриц значений в каждой строке
 		
-		       if(max< sumNegatDetermineMaxRearrangePlaces[i][j]) max = sumNegatDetermineMaxRearrangePlaces[i][j];                           //находим макс значение в каждой строке
+	     if(max< sumNegatDetermineMaxRearrangePlaces[i][j]) max = sumNegatDetermineMaxRearrangePlaces[i][j];                           //находим макс значение в каждой строке
 		 
-		        if(sumNegatDetermineMaxRearrangePlaces[rowMax][colMax]< sumNegatDetermineMaxRearrangePlaces[i][j])  {rowMax = i; colMax = j;}  //находим макс значение матрицы и определением индексы макс значения матрицы 
+	      if(sumNegatDetermineMaxRearrangePlaces[rowMax][colMax]< sumNegatDetermineMaxRearrangePlaces[i][j])  {rowMax = i; colMax = j;}  //находим макс значение матрицы и определением индексы макс значения матрицы 
 		
-		         if(sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] > sumNegatDetermineMaxRearrangePlaces[i][j]) { rowMin = i; colMin = j;}  //находим мин значение матрицы и определением индексов этих значений
-		          }
-		          System.out.print("сумма отрицательных значений в строке " + i + " = " + sum + "; ");	
+		if(sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] > sumNegatDetermineMaxRearrangePlaces[i][j]) { rowMin = i; colMin = j;}  //находим мин значение матрицы и определением индексов этих значений
+		  }
+		   System.out.print("сумма отрицательных значений в строке " + i + " = " + sum + "; ");	
 		
-		           System.out.print("максимальное значение в строке " + i + " = " + max + "; ");
+		     System.out.print("максимальное значение в строке " + i + " = " + max + "; ");
 				
-		            count++;                                                                                                                          //count увеличивается по первому циклу for , вместе с i
+		       count++;                                                                                                                          //count увеличивается по первому циклу for , вместе с i
 		   
-			         System.out.println();
-		              }
-                      System.out.println("_____________________________________________________________________________________");
+			 System.out.println();
+		           }
+                           System.out.println("_____________________________________________________________________________________");
           
-                       System.out.println("maxArray = " + sumNegatDetermineMaxRearrangePlaces[rowMax][colMax] + "; " + "minArray = " + sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] + ". "); 
+                             System.out.println("maxArray = " + sumNegatDetermineMaxRearrangePlaces[rowMax][colMax] + "; " + "minArray = " + sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] + ". "); 
                    
-                        System.out.println("_____________________________________________________________________________________");          
+                               System.out.println("_____________________________________________________________________________________");          
 	
-          for(int i = 0; i< n; i++ ) {
+      for(int i = 0; i< n; i++ ) {
         	 
-  		   for(int j = 0; j< m; j++) {			 
+        for(int j = 0; j< m; j++) {			 
   				
-  			 if(sumNegatDetermineMaxRearrangePlaces[i][j] ==  sumNegatDetermineMaxRearrangePlaces [rowMax][colMax])                              //меняем местами max и min в матрице		
-  			  {
-  			   temp1 = sumNegatDetermineMaxRearrangePlaces [rowMin][colMin]; sumNegatDetermineMaxRearrangePlaces [rowMin][colMin] = sumNegatDetermineMaxRearrangePlaces [rowMax][colMax];  sumNegatDetermineMaxRearrangePlaces [rowMax][colMax] = temp1;
-  			    }   				  				 
-  				 System.out.print(sumNegatDetermineMaxRearrangePlaces[i][j] + " "); 							
-  				  }		  			
- 			      System.out.println();
-  		           }
-                    System.out.println("значения наоборот: maxArray = " + sumNegatDetermineMaxRearrangePlaces[rowMax][colMax] + "; " + "minArray = " + sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] + ". ");     
+          if(sumNegatDetermineMaxRearrangePlaces[i][j] ==  sumNegatDetermineMaxRearrangePlaces [rowMax][colMax])                              //меняем местами max и min в матрице		
+  	    {
+  	    temp1 = sumNegatDetermineMaxRearrangePlaces [rowMin][colMin]; sumNegatDetermineMaxRearrangePlaces [rowMin][colMin] = sumNegatDetermineMaxRearrangePlaces [rowMax][colMax];  sumNegatDetermineMaxRearrangePlaces [rowMax][colMax] = temp1;
+  	      }   				  				 
+  	      System.out.print(sumNegatDetermineMaxRearrangePlaces[i][j] + " "); 							
+  		}		  			
+ 	        System.out.println();
+  		 }
+                  System.out.println("значения наоборот: maxArray = " + sumNegatDetermineMaxRearrangePlaces[rowMax][colMax] + "; " + "minArray = " + sumNegatDetermineMaxRearrangePlaces[rowMin][colMin] + ". ");     
 	}	
 }
 //23 -60 -95 -71 -22 33 51 
